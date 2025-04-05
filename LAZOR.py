890a7deec0c98d808e9456b1ac770c81798ec3b4
yhu-010808 <yhu151@jh.edu>
2025-04-05 15:33:11 -0400

    Update LAZOR.py with new features

--- LAZOR.py
+++ LAZOR.py
@@ -1,19 +1,1 @@
 print("Hello")
-import pickle
-
-# Read pickle file
-file_path = "/Users/huyinan/Desktop/PY/data.pickle" 
-with open(file_path, "rb") as f:
-    data = pickle.load(f)
-
-# show data
-print(type(data))  
-print(data)  
-import pickle
-import matplotlib.pyplot as plt
-
-# 读取 pickle 文件
-file_path = "/Users/huyinan/Desktop/PY/data.pickle" 
-with open(file_path, "rb") as file:
-    data = pickle.load(file)
-# Load the data from the pickle file
