9465a930ebba0847ad08da1d2a00c2ecd9a65bda
Maxine-wang-7 <ywan1006@jh.edu>
2025-04-07 20:16:30 -0400

    Update LAZOR.py with new features

--- LAZOR.py
+++ LAZOR.py
@@ -1,290 +1,198 @@
-class Blocks:
+## Defining 2 global functions
+
+def outputter(mesh):
     """
-    This class defines the 'reflect' and 'transmit' properties for each position in the game board.
+    The result of this function is packaged in an attractive, legible, and intuitive manner.
+    It produces a file called "Solution.bff" that includes a grid that shows how the answer should appear.
+
+    **Parameters**
+
+        mesh : *list,list,tuples*
+            A nested list of strings derived from the grid, indicating all the positions of the blocks as well as the
+            positions/points, through which the lazor can pass through.
+
+    **Returns**
+
+        None
+
+    **Output**
+
+        solution.bff : *file*
+            A .bff file that contains the solution to the input.
     """
 
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
+    print("Analyzing and preparing files for output...")
 
-    def prop(self, meshgrid):
+    solution = []
+
+    for j in range(1,len(mesh), 2):
+        for i in range(1, len(mesh[0]), 2):
+            solution.append(mesh[j][i])
+
+    width = int((len(mesh[0])-1) * 0.5)
+
+    solution = [solution[x:x+width] for x in range(0, len(solution), width)]
+
+    file = open('solution.bff', 'w')
+
+    for i in solution:
+        for j in i:
+            file.write(j)
+            file.write('\t')
+        file.write('\n')
+    file.close()
+
+    print("Solution found")
+
+
+def solution_generator(game_file, max_iter):
+    """
+    This is a function aiming at generating the solution and running the entire program.
+
+    **Parameters**
+        game : *file*
+            The input file which contains the lazor problem to be solved.
+        maxiter : *int*
+            the maximum number of steps of iteration that you want the program to go through.
+            A suitable number is provided below. It breaks if it finds a solution, so the program doesn't run continuously
+            for prolonged times.
+    """
+
+def solution_generator(game_file, max_iter):
+
+
+  for i in range(max_iter):
+      G = Game(game_file)
+      G.database()
+
+      B = Board(G.grid, G.lazor_start, G.lazor_path, G.blocks)
+
+      sample_space = B.get_placeable_positions()
+      placed_grid = B.place_blocks_randomly(sample_space, G.blocks)
+      mesh = B.create_meshgrid(placed_grid)
+
+      L = Laser(G.lazor_start, G.lazor_path)
+      intcp, pth, intercept_new = L.trajectory(G.lazor_path, G.grid, mesh)
+
+      final_set = G.pointer
+      total_intcp = intcp + intercept_new
+
+      if all(x in total_intcp for x in final_set):
+        outputter(mesh)
+        break
+
+
+class MyTest(unittest.TestCase):
+    """
+    A class with all the unit tests
+    """
+
+    def test_game1(self):
         '''
-        This function specifies the 'reflect' and 'transmit' properties as booleans
-        **Parameter**
-            self.x: *int* - position row coordinate
-            self.y: *int* - position column coordinate
-
-        **Returns**
-            self.reflect: *boolean* - whether the position reflects the laser
-            self.transmit: *boolean* - whether the position allows laser to transmit through
+        A function to test the game output and whether the use of functions in the game class
+        is as required.
         '''
+        G1 = Game('dark_1.bff')
+        G1.database()
+        self.assertEqual(G1.grid, [['x', 'o', 'o'], ['o', 'o', 'o'], ['o', 'o', 'x']])
+        self.assertEqual(G1.lazor_path, [(-1, 1), (1, -1), (-1, -1), (1, -1)])
 
-        if meshgrid[self.y][self.x] == 'A':
-            self.reflect = True
-            self.transmit = False
-        elif meshgrid[self.y][self.x] == 'B':
-            self.reflect = False
-            self.transmit = False
-        elif meshgrid[self.y][self.x] == 'C':
-            self.reflect = True
-            self.transmit = True
-        else:
-            self.reflect = False
-            self.transmit = True
-        return self.reflect, self.transmit
+    def test_game2(self):
+        '''
+        A function to test the game output and whether the use of functions in the game class
+        is as required.
+        '''
+        G2 = Game('tiny_5.bff')
+        G2.database()
+        self.assertEqual(G2.blocks, {'A': 3, 'C': 1, 'B': 0})
+        self.assertEqual(G2.pointer, [(1, 2), (6, 3)])
 
+    def test_game3(self):
+        '''
+        A function to test the game output and whether the use of functions in the game class
+        is as required.
+        '''
+        G3 = Game('yarn_5.bff')
+        G3.database()
+        self.assertEqual(G3.lazor_start, [(4, 1)])
+        self.assertEqual(G3.grid, [['o', 'B', 'x', 'o', 'o'], [
+            'o', 'o', 'o', 'o', 'o'], ['o', 'x', 'o', 'o', 'o'], ['o', 'x', 'o', 'o', 'x'], ['o', 'o', 'x', 'x', 'o'], [
+            'B', 'o', 'x', 'o', 'o']])
 
-class Laser:
-    """
-    The laser class stores the laser's starting point and orientation.
-    It also includes capabilities that enable the laser beam to navigate the board
-    and identify certain blocks.
-    """
+    def test_sampler(self):
+        '''
+        A function to check if the sampler samples the right set of vacant positions or not.
+        '''
+        G = Game('mad_4.bff')
+        G.database()
+        B = Board(G.grid,G.lazor_start, G.lazor_path,G.pointer)
+        self.assertEqual(B.sampler(G.grid), [(0, 0), (1, 0), (2, 0), (3, 0), (0, 1), (1, 1), (2, 1),
+            (3, 1), (0, 2), (1, 2), (2, 2), (3, 2), (0, 3), (1, 3), (2, 3), (3, 3), (0, 4), (1, 4), (2, 4), (3, 4)])
 
-    def __init__(self, start_point, path):
-        self.source = start_point
-        self.direction = path
+    def test_blocks_1(self):
+        '''
+        A function to check if the Blocks class can distinguish between the Blocks correctly and output apt booleans.
+        '''
+        G = Game('tiny_5.bff')
+        G.database()
+        B = Board(G.grid,G.lazor_start, G.lazor_path,G.pointer)
+        mesh_board = B.sample_board(B.sampler(G.grid), G.blocks, G.grid)
+        mesh = B.make_board(mesh_board)
+        (a,b) = Blocks(3,1).prop(mesh)
+        self.assertFalse(a)
+        self.assertFalse(b)
 
-    def pos_chk(x, y, nBlocks):
+    def test_blocks_2(self):
+        '''
+        A function to check if the Blocks class can distinguish between the Blocks correctly and output apt booleans.
+        '''
+        G = Game('yarn_5.bff')
+        G.database()
+        B = Board(G.grid,G.lazor_start, G.lazor_path,G.pointer)
+        mesh_board = B.sample_board(B.sampler(G.grid), G.blocks, G.grid)
+        mesh = B.make_board(mesh_board)
+        (a,b) = Blocks(1,11).prop(mesh)
+        self.assertFalse(a)
+        self.assertFalse(b)
+
+    def test_lazer1(self):
         """
-        To determine whether the coordinates are within the grid or not.
-
-        **Parameters**
-
-            x : *int*
-                Denotes the x-coordinate
-            y : *int*
-                Denotes the y-coordinate
-            nBlocks : *int*
-                Denotes the length of the box.
+        A function with a sample test file that checks if the laser class performs the trajectory accurately.
         """
-        return x >= 0 and x < nBlocks and y >= 0 and y < nBlocks
-
-    def laser_strikes(self, path, intercepts, grid, meshgrid, path_1, intercept_new):
-        """
-        The function for predicting where a lazor beam will strike.
-        This function analyzes each point of the laser beam.
-        The algorithm will identify block neighbors, analyze their features, and conduct appropriate operations on them.
-
-        **Parameters**
-            path : *list, list, tuple*
-                A nested list of tuples containing the direction of the lazor.
-            intercepts : *list, list, tuple*
-                A nested list of tuples containing the intercepts through which the lazor beam passes through.
-            grid : *list,list,str*
-                A nested list of strings that denotes the positioning of the blocks in the game. Subset of meshgrid
-            meshgrid : *list,list,str*
-                A nested list of strings derived from the grid, indicating all the positions of the blocks as well as the
-                positions/points, through which the lazor can pass through.
-
-        **Returns**
-            path : *list,list,tuples*
-                A nested list of tuples with all the directions at each intercept.
-            intercepts : *list,list,tuples*
-                A nested list of tuples with all the intercepts that the laser beam traversed through.
-            path_1 : *list,tuples*
-                A list of tuples indicating the directions after laser splits at the refract blocks.
-            intercept_new : *list,tuples*
-                A list of tuples indicating the intercepts that the laser parses through after being split.
-
-        """
-
-        (dx, dy) = path[-1]
-
-        # The last position of the laser
-        (nx, ny) = intercepts[-1]
-
-
-        # Directions to perform a check
-        n_direct = [(0, 1),(0, -1),(-1, 0),(1, 0)]
-
-
-        # Creating a buffer to check the surrounding positions
-        nlist = []
-        transmit_list = []
-
-        # Exploring the neighbours of the new point to modify directions
-
-        if (dx,dy) != (0,0):
-
-        #This will allow the loop to proceed only if there is a viable direction to proceed
-
-            for i in range(len(n_direct)):
-
-                ex = nx + n_direct[i][0]
-                ey = ny + n_direct[i][1]
-
-                if ex > 0 and ex < 2*len(grid[0])+1 and ey > 0 and ey < 2*len(grid)+1:
-                    #Just to perform a check that we are still within the grid
-                    delta_x = ex-nx
-                    delta_y = ey-ny
-
-
-
-                    if meshgrid[ey][ex] == 'A':
-
-                        if delta_x == 0:
-                            new_dx = dx * 1
-                        else:
-                            new_dx = dx * -1
-                        if delta_y == 0:
-                            new_dy = dy * 1
-                        else:
-                            new_dy = dy * -1
-                        nlist.append((new_dx,new_dy))
-
-
-                    elif Blocks(ex,ey).prop(meshgrid) == (False,False) :
-
-
-                        if delta_x == dx or delta_y == dy:
-                            new_dx = dx * 0
-                            new_dy = dy * 0
-                        else :
-                            new_dx = dx * 1
-                            new_dy = dy * 1
-                        nlist.append((new_dx,new_dy))
-
-
-                    elif Blocks(ex,ey).prop(meshgrid) == (True,True) :
-
-
-                        if delta_x == dx or delta_y == dy:
-
-                            if delta_x == 0:
-                                new_dx = dx * 1
-                            else:
-                                new_dx = dx * -1
-                            if delta_y == 0:
-                                new_dy = dy * 1
-                            else:
-                                new_dy = dy * -1
-                            old_dx = dx
-                            old_dy = dy
-                            transmit_list.append((old_dx,old_dy))
-                            nlist.append((new_dx,new_dy))
-
-                        else:
-
-                            new_dx = dx * 1
-                            new_dy = dy * 1
-                            nlist.append((new_dx,new_dy))
-
-
-            if len(nlist) > 0:
-
-                path.append(nlist[-1])
-
-            else :
-                path.append((dx,dy))
-
-            if len(transmit_list) > 0 :
-                path_1.append(transmit_list[-1])
-                intercept_new.append((nx,ny))
-
-            nx += path[-1][0]
-            ny += path[-1][1]
-
-            intercepts.append((nx,ny))
-
-
-        return path,intercepts, path_1, intercept_new
-
-
-    def trajectory(self, path, grid, meshgrid):
-        """
-        This function invokes the laser_strikes function and then attempts to determine the trajectory,
-        assuming the laser has feasible directions.
-
-        **Parameters**
-
-            path : *list, list, tuple*
-                A nested list of tuples containing the direction of the lazor.
-            grid : *list,list,str*
-                A nested list of strings that denotes the positioning of the blocks in the game. Subset of meshgrid
-            meshgrid : *list,list,str*
-                A nested list of strings derived from the grid, indicating all the positions of the blocks as well as the
-                positions/points, through which the lazor can pass through.
-
-        **Returns**
-
-            final_intercept_list : *list,tuples*
-                A list of tuples containing the intercepts through which the laser/s pass/es. Doesn't include the intercepts
-                cause by the laser after split.
-            path : *list,tuples*
-                A nested list of path for all the laser beams.
-            intercept_new : *list,tuples*
-                A list of tuples containing the intercepts through which the laser beam passes after being split at a refract block.
-        """
-
-        intercepts = []
-        path = []
-
-        for i in range(len(self.source)):
-            intercepts.append([self.source[i]])
-            path.append([self.direction[i]])
-
-
-        n_direct = [(0, 1),(0, -1),(-1, 0),(1, 0)]
-
-        path_1 = []
-        intercept_new = []
-
-
-        for k in range(len(path)) :
-
-            # Perform an initial check at the input location to verify that if there is a block in the neighbour domain
-            # of the origin source, the laser's direction changes immediately.
-
-            if len(intercepts[k]) == 1:
-
-                path[k], intercepts[k], path_1, intercept_new = self.laser_strikes(path[k], intercepts[k], grid, meshgrid, path_1, intercept_new)
-
-            # To continue the loop till the laser reaches the boundary of the grid.
-
-            while intercepts[k][-1][0] != 0 and intercepts[k][-1][0] < len(meshgrid[0])-1 and intercepts[k][
-            -1][1] != 0 and intercepts[k][-1][1] < len(meshgrid)-1:
-
-
-                if path[k][-1] != (0,0) :
-
-                    path[k], intercepts[k], path_1, intercept_new = self.laser_strikes(path[k], intercepts[k], grid, meshgrid, path_1, intercept_new)
-
-                else :
-
-                    break
-
-            # Check to see whether the Laser hits any refract blocks, as it will split on impact and take two courses later on.
-            # The next 'if' conditional will allow you to explore an extra path using the laser's origin at the split point.
-            # It uses the function laser_strikes to investigate the transmit path, given that the split interface is now the origin.
-
-            if len(path_1) != 0:
-
-                path_0 = []
-                intercept_0 = []
-
-                (dx,dy) = path_1[-1]
-                (cx, cy) = intercept_new[-1]
-
-                nx = cx + dx
-                ny = cy + dy
-
-                intercept_new.append((nx,ny))
-
-                while intercept_new[-1][0] != 0 and intercept_new[-1][0] < len(meshgrid[0])-1 and intercept_new[
-                -1][1] != 0 and intercept_new[-1][1] < len(meshgrid)-1:
-
-
-                    if path_1[-1] != (0,0) :
-
-                        path_1, intercept_new, path_0, intercept_0 = self.laser_strikes(path_1, intercept_new, grid, meshgrid, path_0, intercept_0)
-
-                    else:
-                        break
-
-        final_intercept_list = []
-        for sublist in intercepts:
-            for item in sublist:
-                final_intercept_list.append(item)
-
-        return final_intercept_list, path, intercept_new
+        G = Game('unit_test_sample.bff')
+        G.database()
+        B = Board(G.grid,G.lazor_start, G.lazor_path,G.pointer)
+        mesh_board = B.sample_board(B.sampler(G.grid), G.blocks, G.grid)
+        mesh = B.make_board(mesh_board)
+        L = Laser(G.lazor_start,G.lazor_path)
+        intcp, pth, intercept_new = L.trajectory(G.lazor_path,G.grid, mesh)
+        self.assertEqual(intcp, [(7, 2), (6, 1), (5, 0)])
+        self.assertEqual(intercept_new, [(7, 2), (6, 3), (5, 4), (4, 3), (3, 2), (2, 1), (1, 0)])
+        self.assertEqual(pth, [[(-1, 1), (-1, -1), (-1, -1)]])
+
+    def test_lazer2(self):
+        '''
+        A function with a sample test file that checks if the laser class performs the trajectory and laser_strikes
+        operations accurately.
+        This function additionally checks if the output is right or not.
+        '''
+        G = Game('unit_test_sample.bff')
+        G.database()
+        B = Board(G.grid,G.lazor_start, G.lazor_path,G.pointer)
+        mesh_board = B.sample_board(B.sampler(G.grid), G.blocks, G.grid)
+        mesh = B.make_board(mesh_board)
+        L = Laser(G.lazor_start,G.lazor_path)
+        intcp, pth, intercept_new = L.trajectory(G.lazor_path,G.grid, mesh)
+        total_intcp = intcp + intercept_new
+        final_set = G.pointer
+        self.assertTrue(all(x in total_intcp for x in final_set))
+
+if __name__ == '__main__':
+
+  solution_generator('mad_4.bff', 500000)
+
+
+
+
+    # The following line performs unit tests
+  unittest.main()
